8f5d4477803e11fe785a61840774194a712de4df
Hunar Batra <i@hunarbatra.com>
2023-06-08 16:20:32 +0100

    changes

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, request
+from flask_cors import CORS
 import requests
 
+# from flask_ngrok import run_with_ngrok
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/proxy": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+
+# run_with_ngrok(app)
 
 @app.route('/proxy', methods=['GET', 'POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def proxy():
     if request.method == 'POST':
         req_data = request.get_json()
